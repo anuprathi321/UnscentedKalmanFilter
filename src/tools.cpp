fc026e3ec25b8410d302e55eff2fd32df0868bac
anuprathi321 <anuprathi321@gmail.com>
2018-06-23 08:31:01 -0700

    update

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,25 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	if(estimations.size() == 0 || estimations.size() != ground_truth.size())
+		return rmse;
+
+	for(unsigned int i = 0; i < estimations.size(); i++)
+	{
+		VectorXd resd = ground_truth[i] - estimations[i];
+
+		resd = resd.array() * resd.array();
+		rmse = rmse + resd;
+	}
+
+	rmse = rmse/estimations.size();
+	rmse = sqrt(rmse.array());
+
+	return rmse;
+
 }
